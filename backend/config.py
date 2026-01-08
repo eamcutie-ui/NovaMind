ed2b2e8722eb2200c7165b7270e3488dc8c4b36f
eamcutie-ui <eamcutie@gmail.com>
2026-01-08 21:55:32 +0530

    Add configuration settings for NovaMind V7

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+# NovaMind V7 Config (Elite)
+DAILY_LIMIT_FREE = 50
+DAILY_LIMIT_PRO = 1000
+AI_VERSION = "V7-MaxElite"
+TOOLS_AVAILABLE = ["chat","text","image","fun","idea"]
+DEFAULT_THEME = "dark"
+MAX_HISTORY = 100
